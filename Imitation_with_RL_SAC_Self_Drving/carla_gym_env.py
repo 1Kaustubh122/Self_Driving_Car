36f34a5df11019fd70ba0cfcbd9f31d2bedb44ec
Kaustubh Krishna <kkaustubh2006@gmail.com>
2025-04-19 15:17:35 +0530

    Improved reward function

---
+++ Imitation_with_RL_SAC_Self_Drving/carla_gym_env.py
@@ -0,0 +1,211 @@
+import torch
+import carla
+import numpy as np
+import gymnasium as gym
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from Imitation_Learning_RL.process_frame import process_frame
+
+
+def pre_process(rgb_image, seg_image, transform):
+    
+    if transform:
+        rgb_tensor = transform(rgb_image)
+    else:
+        rgb_tensor = transforms.ToTensor()(rgb_image)
+        
+    seg_image = np.array(seg_image)
+    
+    lane_mask = np.all(seg_image == [0, 255, 0], axis=2).astype(np.uint8)
+    obs_mask = np.all(seg_image == [255, 0, 0], axis=2).astype(np.uint8)
+    
+    seg_tensor = torch.tensor(np.stack([lane_mask, obs_mask], axis=0), dtype=torch.float32)
+    
+    if seg_tensor.shape[1:] != rgb_tensor.shape[1:]:
+        seg_tensor = F.interpolate(
+            seg_tensor.unsqueeze(0),
+            size=rgb_tensor.shape[1:],
+            mode='nearest'
+        ).squeeze(0)
+    
+    input_tensor = torch.cat([rgb_tensor, seg_tensor], dim=0)
+    
+    return input_tensor
+
+class CarlaEnv(gym.Env):
+    def __init__(self):
+        super(CarlaEnv, self).__init__()
+        
+        self.client_ = carla.Client('localhost', 2000)
+        self.client_.set_timeout(10.0)
+        self.world_ = self.client_.get_world()
+        self.blueprint_library_ = self.world_.get_blueprint_library()
+
+                        # Steer, Throttle, Brake
+        self.action_space_ = gym.spaces.Box(
+            low= np.array([-1.0, 0.0, 0.0]),
+            high= np.array([1.0, 1.0, 1.0]),
+            dtype=np.float32
+        )
+        
+                                # 5 Channel image (244 x 244)
+        self.observation_space = gym.spaces.Box(
+            low=0, 
+            high=255,
+            shape=(5, 640, 360), 
+            dtype=np.uint8
+        )
+        
+        self.vehicle_ = None
+        self.camera_sensor = None
+        self.collision_sensor = None
+        self.lane_invasion_sensor = None
+        
+        self.max_steps = 1000
+        self.episode_step = 0
+        self.previous_location = None
+        self.waypoints = []
+        self.current_waypoint_idx = 0
+        
+        self.rgb_image = None
+        self.seg_image = None
+        self.collision = False
+        self.lane_invasion = False
+        
+    # Sensors Callbacks
+    def image_callback(self, data):
+        array = np.frombuffer(data.raw_data, dtype=np.uint8)
+        array = np.reshape(array, (data.height, data.width, 4))[:, :, :3]
+        self.rgb_image = array
+        self.seg_image = process_frame(self.rgb_image)
+
+    def collision_callback(self, data):
+        self.collision = True
+
+    def lane_invasion_callback(self, data):
+        self.lane_invasion = True
+    
+    def reset(self, seed = None, options = None):
+        if seed is not None:
+            np.random.seed(seed)
+            
+        if self.vehicle_ is not None:
+            self.vehicle_.destroy()
+            
+        for sensor in [self.camera_sensor, self.collision_sensor, self.lane_invasion_sensor]:
+            if sensor is not None:
+                sensor.destroy()
+                
+                
+        spawn_point = self.world_.get_map().get_spawn_points()[0]
+                
+        vehicle_bp_ = self.blueprint_library_.find('vehicle.volkswagen.t2_2021')
+        camera_bp_ = self.blueprint_library_.find('sensor.camera.rgb')
+        collision_bp = self.blueprint_library_.find("sensor.other.collision")
+        lane_inv_bp = self.blueprint_library_.find("sensor.other.lane_invasion")
+        
+        camera_bp_.set_attribute('image_size_x', '640')
+        camera_bp_.set_attribute('image_size_y', '360')
+        
+        self.vehicle_ = self.world_.try_spawn_actor(vehicle_bp_, spawn_point)
+    
+        camera_location = carla.Location(x=4.442184 / 2.9, y=0, z=2.2)
+        
+        camera_transform = carla.Transform(camera_location)
+        collision_transform = carla.Transform()
+        lane_inv_transform = carla.Transform()
+        
+        self.camera_sensor = self.world_.spawn_actor(camera_bp_, camera_transform, attach_to=self.vehicle_)
+        self.collision_sensor = self.world_.spawn_actor(collision_bp, collision_transform, attach_to=self.vehicle_)
+        self.lane_invasion_sensor = self.world_.spawn_actor(lane_inv_bp, lane_inv_transform, attach_to=self.vehicle_)
+        
+        self.camera_sensor.listen(lambda data: self.image_callback(data))
+        self.collision_sensor.listen(lambda data: self.collision_callback(data))
+        self.lane_invasion_sensor.listen(lambda data: self.lane_invasion_callback(data))
+        
+        self.episode_step = 0
+        self.collision = False
+        self.lane_invasion = False
+        self.previous_location = self.vehicle_.get_location()
+        
+        map_ = self.world_.get_map()
+        self.waypoints = [map_.get_waypoint(spawn_point.location)]
+        for _ in range(50):
+            next_wp = self.waypoints[-1].next(5.0)
+            self.waypoints.append(next_wp[0] if next_wp else self.waypoints[-1])
+        self.current_waypoint_idx = 0
+        
+        return self.get_observation(), {}
+    
+    def get_observation(self):
+        while self.rgb_image is None or self.seg_image is None:
+            self.world_.tick()
+        
+        obs = pre_process(self.rgb_image, self.seg_image, transform = None).numpy()
+        return obs
+    
+    def step(self, action):
+        
+        steer_, throttle_, brake_  = action
+        # brake_ = 1.0 if brake_ > 0.65 else 0.0
+        brake = max(min(brake_, 1.0), 0.0)
+        
+        self.vehicle_.apply_control(carla.VehicleControl(
+            throttle = float(throttle_),
+            steer = float(steer_),
+            brake = float(brake)
+        ))
+        self.world_.tick()
+        
+        obs = self.get_observation()
+        reward = self.compute_reward()
+        self.episode_step += 1
+        
+        done = (
+            self.collision or
+            # self.lane_invasion or    ## Add later give time to learn first
+            self.episode_step >= self.max_steps or
+            self.current_waypoint_idx >= len(self.waypoints) - 1
+        )
+        
+        truncated = self.episode_step >= self.max_steps
+        
+        return obs, reward, done or truncated, truncated, {}
+        
+    def compute_reward(self):
+        reward = 0
+        
+        current_location = self.vehicle_.get_location()
+       
+        while self.current_waypoint_idx < len(self.waypoints) - 1:
+            wp = self.waypoints[self.current_waypoint_idx]
+            if current_location.distance(wp.transform.location) < 2.0:
+                self.current_waypoint_idx += 1
+                reward += 10.0
+            else:
+                break
+                
+        if self.collision:
+            reward -= 100.0
+        if self.lane_invasion:
+            reward -= 20.0
+        
+        map_ = self.world_.get_map()
+        wp = map_.get_waypoint(current_location)
+        
+        if self.current_waypoint_idx < len(self.waypoints):
+            current_wp = self.waypoints[self.current_waypoint_idx]
+            prev_dist = self.previous_location.distance(current_wp.transform.location)
+            curr_dist = current_location.distance(current_wp.transform.location)
+            reward += max(prev_dist - curr_dist, 0) * 0.1
+        self.previous_location = current_location
+        
+        velocity = self.vehicle_.get_velocity()
+        speed = np.linalg.norm([velocity.x, velocity.y, velocity.z])
+        reward += speed * 0.1
+        
+        if speed < 0.2:
+            reward -= 5.0
+
+
+        reward = np.clip(reward, -100, 50.0)
